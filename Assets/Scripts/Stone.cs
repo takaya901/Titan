cfa4bed26d25fab9d1d64161f74eb889f7e4521b
Takaya Ogawa <bumpofchicken344@gmail.com>
2021-12-25 21:58:04 +0900

    Add ThrowStone

---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    [SerializeField] float _flightTime = 1f;
+
+    void Start()
+    {
+        StartCoroutine(Throw(Camera.main.transform.position + Vector3.forward));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // https://www.gocca.work/unity-parabolic-movement/
+    // 現在位置からendPosへの放物運動
+    IEnumerator Throw(Vector3 endPos)
+    {
+        var gravity = -9.8f;
+        var speedRate = 1f;
+        var startPos = transform.position; // 初期位置
+        var diffY = (endPos - startPos).y; // 始点と終点のy成分の差分
+        var vn = (diffY - gravity * 0.5f * _flightTime * _flightTime) / _flightTime; // 鉛直方向の初速度vn
+
+        // 放物運動
+        for (var t = 0f; t < _flightTime; t += (Time.deltaTime * speedRate))
+        {
+            var p = Vector3.Lerp(startPos, endPos, t / _flightTime);   //水平方向の座標を求める (x,z座標)
+            p.y = startPos.y + vn * t + 0.5f * gravity * t * t; // 鉛直方向の座標 y
+            transform.position = p;
+            yield return null; //1フレーム経過
+        }
+        // 終点座標へ補正
+        transform.position = endPos;
+    }
+}
---
+++ Assets/Scripts/Titan.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Titan : MonoBehaviour
+{
+    [SerializeField] GameObject _stonePrefab;
+    Animator _anim;
+
+    void Start()
+    {
+        _anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            ThrowStone();
+        }
+    }
+
+    void ThrowStone()
+    {
+        _anim.SetTrigger("Throw");
+        _anim.SetTrigger("Idle");
+        var initPos = transform.position + new Vector3(0f, transform.localScale.y*0.8f, 0f);
+        var stone = Instantiate(_stonePrefab, initPos, Quaternion.identity);
+    }
+}
