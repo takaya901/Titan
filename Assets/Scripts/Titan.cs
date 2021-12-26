0a98c6719670938c2003a385bd87012b3b9255e3
Takaya Ogawa <bumpofchicken344@gmail.com>
2021-12-26 15:22:36 +0900

    Add manual move of titan

--- Assets/Scripts/Titan.cs
+++ Assets/Scripts/Titan.cs
@@ -5,7 +5,12 @@ using static UnityEngine.Input;
 
 public class Titan : MonoBehaviour
 {
+    [SerializeField] float _moveSpeed = 0.1f;
+    [SerializeField] MoveType _moveType;
     [SerializeField] GameObject _stonePrefab;
+
+    Vector2 _touchStart;     //タッチ開始時座標
+    Vector2 _touchEnd;       //タッチ終了時座標
     Animator _anim;
 
     void Start()
@@ -21,8 +26,54 @@ public class Titan : MonoBehaviour
 
     void Update()
     {
+        Move();
         if (GetKeyDown("space") || touchCount > 0){
             ThrowStone();
+        }
+    }
+
+    /// <summary>
+    /// MoveTypeで指定された平面状を移動する
+    /// </summary>
+    void Move()
+    {
+        // タッチ開始の座標を保存
+        if (GetMouseButtonDown(0)) {
+            _touchStart = mousePosition;
+        }
+
+        // タッチ中はタッチ位置の移動量分だけモデルの座標を動かす
+        if (GetMouseButton(0))
+        {
+            _touchEnd = mousePosition;
+
+            Vector3 diff = Vector3.zero;
+
+            switch (_moveType)
+            {
+                case MoveType.Xy:
+                    diff = _touchEnd - _touchStart;
+                    //diff.x = -diff.x;
+                    break;
+                //case MoveType.Xz:
+                //    //diff = (_touchEnd - _touchStart).ToVector3xz();
+                //    diff = -diff;
+                //    break;
+                //default:
+                    //throw new ArgumentOutOfRangeException();
+            }
+
+            transform.position += diff * _moveSpeed;
+
+            // 現在の座標を保存
+            _touchStart = mousePosition;
+        }
+
+        // タッチ終了時に座標をリセット
+        if (GetMouseButtonUp(0))
+        {
+            _touchStart = Vector2.zero;
+            _touchEnd = Vector2.zero;
         }
     }
 
@@ -31,5 +82,11 @@ public class Titan : MonoBehaviour
         _anim.SetTrigger("Attack");
         var initPos = transform.position + new Vector3(0f, transform.localScale.y*0.8f, 0f);
         var stone = Instantiate(_stonePrefab, initPos, Quaternion.identity);
+    }
+
+    enum MoveType
+    {
+        Xy,
+        Xz
     }
 }
