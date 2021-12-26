446967bc9d766e7df0ae8a5d3293e01287a92c25
Takaya Ogawa <bumpofchicken344@gmail.com>
2021-12-26 16:39:37 +0900

    Add Bullet, the base class

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Input;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject _bulletPrefab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (GetKeyDown("space") || touchCount > 0){
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        //// レーザー（ray）を飛ばす「起点」と「方向」
+        //Ray ray = new Ray(transform.position, transform.forward);
+        //RaycastHit hit;
+
+        //if (Physics.Raycast(ray, out hit, 60))
+        //{
+        //    var hitName = hit.transform.gameObject.tag;
+
+        //    if (hitName == "Enemy")
+        //    {
+        //        // 照準器の色を「赤」に変える（色は自由に変更してください。）
+        //        aimImage.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        //    }
+        //    else
+        //    {
+        //        // 照準器の色を「水色」（色は自由に変更してください。）
+        //        aimImage.color = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+        //    }
+        //}
+        //else
+        //{
+        //    // 照準器の色を「水色」（色は自由に変更してください。）
+        //    aimImage.color = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+        //}
+
+        var bullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+        bullet.GetComponent<Bullet>().TargetPos = new Vector3(0f, 0f, 20f);
+    }
+}
--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -3,50 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using NativeUtil;
 
-public class Stone : MonoBehaviour
+public class Stone : Bullet
 {
-    [SerializeField] float _flightTime = 1f;
-    [SerializeField] Material _transparent;
-    Vector3 _endPos;
-    AudioSource _audioSource;
-
-    void Start()
+    new void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
-        _endPos = Camera.main.transform.position + new Vector3(0f, 0f, 0.5f);
-        StartCoroutine(Throw(_endPos));
+        base.Start();
+        TargetPos = Camera.main.transform.position + new Vector3(0f, 0f, 0.5f);
     }
 
-    void Update()
-    {
-
-    }
-
-    // https://www.gocca.work/unity-parabolic-movement/
-    // 現在位置からendPosへの放物運動
-    IEnumerator Throw(Vector3 endPos)
-    {
-        var gravity = -9.8f;
-        var speedRate = 1f;
-        var startPos = transform.position; // 初期位置
-        var diffY = (endPos - startPos).y; // 始点と終点のy成分の差分
-        var vn = (diffY - gravity * 0.5f * _flightTime * _flightTime) / _flightTime; // 鉛直方向の初速度vn
-
-        // 放物運動
-        for (var t = 0f; t < _flightTime; t += (Time.deltaTime * speedRate))
-        {
-            var p = Vector3.Lerp(startPos, endPos, t / _flightTime);   //水平方向の座標を求める (x,z座標)
-            p.y = startPos.y + vn * t + 0.5f * gravity * t * t; // 鉛直方向の座標 y
-            transform.position = p;
-            yield return null; //1フレーム経過
-        }
-        // 終点座標へ補正
-        transform.position = endPos;
-    }
-
+    /// <summary>
+    /// プレイヤーにヒットした時
+    /// </summary>
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("MainCamera"))
+        if (other.CompareTag("Player"))
         {
             gameObject.GetComponent<Renderer>().material = _transparent;
             _audioSource.Play();
