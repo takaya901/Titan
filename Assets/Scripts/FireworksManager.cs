52234a13f6209eb8e7a4ad940c670a193319cce6
Takaya Ogawa <bumpofchicken344@gmail.com>
2022-01-02 11:06:48 +0900

    Add fireworks to clear scene

---
+++ Assets/Scripts/FireworksManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireworksManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] _rockets;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(6f);
+        foreach (var rocket in _rockets) {
+            rocket.SetActive(true);
+        }
+    }
+}
